ad0824f578ff101c8ab9153da08e90bf4fb3d1a9
JC Brand <jc@opkode.com>
2011-12-01 23:53:25 +0200

    Add a new view that gets called when a query is submitted.
    The form is stored in the current session.

--- slc/facetedcalendar/browser/view.py
+++ slc/facetedcalendar/browser/view.py
@@ -1,5 +1,8 @@
 from zope.component import getMultiAdapter
 from Acquisition import aq_inner
+from persistent.dict import PersistentDict
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
 from Solgema.fullcalendar import interfaces
 from Solgema.fullcalendar.browser.views import SolgemaFullcalendarView
 from Solgema.fullcalendar.browser.actions import BaseActionView 
@@ -7,12 +10,26 @@ from Solgema.fullcalendar.browser.actions import BaseActionView
 class FacetedCalendarView(SolgemaFullcalendarView):
     """ """
 
-    def queryCatalog(self):
+    def get_results(self):
+        """ """
         context = aq_inner(self.context)
         source = getMultiAdapter(
                         (self.context, self.request),
                         interfaces.IEventSource)
         return source.getFacetedEvents()
+
+
+class UserQuerySubmitted(BrowserView):
+    
+    def __call__(self):
+        """ """
+        sdm = getToolByName(self.context, 'session_data_manager')
+        session = sdm.getSessionData(create=True)
+        path = '/'.join(self.context.getPhysicalPath())
+        pdict = PersistentDict()
+        pdict.update(self.request.form)
+        session.set(path, pdict) 
+        return self.request.RESPONSE.redirect(path)
 
 
 class PropertiesGuard(BaseActionView):
