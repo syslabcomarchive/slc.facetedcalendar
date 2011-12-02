7d5c17b1c6a688626fe63cd5155b360d4ce40faa
JC Brand <jc@opkode.com>
2011-12-02 10:24:52 +0200

    Add a new interface with schema for the query box config form.

--- slc/facetedcalendar/browser/interfaces.py
+++ slc/facetedcalendar/browser/interfaces.py
@@ -1,4 +1,7 @@
+from zope import schema
 from zope.interface import Interface
+from z3c.form import button
+from slc.facetedcalendar import MessageFactory as _
 
 class IAjaxView(Interface):
     """ """
@@ -6,3 +9,18 @@ class IAjaxView(Interface):
     def render_faceted_parameters(self, start, end):
         """ """
 
+class IFacetsConfigForm(Interface):
+    """ """
+    facets = schema.Set(
+                title=_(u"Filter Parameters"),
+                description=_(u"Choose which parameters a user can use to "
+                    u"filter the calendar results."),
+                value_type=schema.Choice(
+                    vocabulary="slc.facetedcalendar.vocabularies.facetedcalendarparameters"
+                ),
+            )
+    save = button.Button(title=_(u"Save"))
+
+
+class IFacetsConfigView(Interface):
+    """ """
